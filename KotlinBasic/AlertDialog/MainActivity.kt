2489e619ebda7bf75a63aabeac4f6e0d08e30012
DIperera <IT23590762@my.sliit.lk>
2025-07-05 12:43:29 +0530

    Update MainActivity.kt

--- KotlinBasic/AlertDialog/MainActivity.kt
+++ KotlinBasic/AlertDialog/MainActivity.kt
@@ -1,33 +1,63 @@
+// Package name for the project
 package com.example.alertdialogexample
 
+// Importing necessary Android libraries
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
+// This is the main activity class of the app, where the UI starts
 class MainActivity : AppCompatActivity() {
+
+    // This function runs when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Set the layout for this activity (activity_main.xml)
         setContentView(R.layout.activity_main)
 
+        // Find the Button from the XML layout using its ID
         val showDialogBtn = findViewById<Button>(R.id.showDialogButton)
 
+        // Set a click listener on the button
         showDialogBtn.setOnClickListener {
-            // Create and show AlertDialog
+
+            // Create an AlertDialog builder using the current activity as context
             val builder = AlertDialog.Builder(this)
+
+            // Set the title of the dialog
             builder.setTitle("Confirm Action")
+
+            // Set the message (main text) of the dialog
             builder.setMessage("Are you sure you want to continue?")
+
+            // Set the action for the "Yes" button
             builder.setPositiveButton("Yes") { dialog, _ ->
+                // Show a short message (Toast) when "Yes" is clicked
                 Toast.makeText(this, "You clicked Yes", Toast.LENGTH_SHORT).show()
+
+                // Close the dialog
                 dialog.dismiss()
             }
+
+            // Set the action for the "No" button
             builder.setNegativeButton("No") { dialog, _ ->
+                // Show a message when "No" is clicked
                 Toast.makeText(this, "You clicked No", Toast.LENGTH_SHORT).show()
+
+                // Close the dialog
                 dialog.dismiss()
             }
-            builder.setCancelable(false) // prevents closing when touching outside
+
+            // Make sure the dialog cannot be dismissed by tapping outside it
+            builder.setCancelable(false)
+
+            // Create the actual AlertDialog from the builder
             val alertDialog = builder.create()
+
+            // Finally, show the dialog on the screen
             alertDialog.show()
         }
     }
